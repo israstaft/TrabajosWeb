55a3de10a4dba45c31f25e6f7b14cf6f9957a11d
Israel Camilo <israeladriannunez14@gmail.com>
2020-10-22 12:28:07 -0600

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,26 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { EnviarCorreoComponent } from './auth/enviar-correo/enviar-correo.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { CreartrabajosComponent } from './principales/creartrabajos/creartrabajos.component';
+import { VertrabajosComponent } from './principales/vertrabajos/vertrabajos.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    EnviarCorreoComponent,
+    FilterPipe,
+    CreartrabajosComponent,
+    VertrabajosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
