1af51e8000c3b9353d5b284e936f3da0c054ad7a
Israel Camilo <israeladriannunez14@gmail.com>
2020-10-22 17:09:41 -0600

    agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { EnviarCorreoComponent } from './auth/enviar-correo/enviar-correo.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { CreartrabajosComponent } from './principales/creartrabajos/creartrabajos.component';
+import { VertrabajosComponent } from './principales/vertrabajos/vertrabajos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'regitro',
+    component: RegistroComponent
+  },
+  {
+    path: 'vertrabajos',
+    component: VertrabajosComponent
+  },
+  {
+    path: 'creartrabajos',
+    component: CreartrabajosComponent
+  },
+  {
+    path: 'verificacion-email',
+    component: EnviarCorreoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
